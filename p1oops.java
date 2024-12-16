5976eebe51954677a097868385f4c90c48736c21
Somnath474 <somnathgupta474@gmail.com>
2024-12-16 23:21:57 +0530

    Add files via upload

---
+++ p1oops.java
@@ -0,0 +1,11 @@
+public class p1oops {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.name="Somnath";
+        System.out.println(s.name);
+    }
+}
+class Student{
+String name;
+int marks;
+}
---
+++ statickeyword.java
@@ -0,0 +1,4 @@
+public class statickeyword {
+    //one time declared in class then can be used in every sub class without repetition 
+    //saves memory
+}
